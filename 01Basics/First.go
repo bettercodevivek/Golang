dadceb6f62f7893dbfd946216a47e9e66ad4d1ce
bettercodevivek <viveksingh0520@gmail.com>
2024-09-19 12:38:14 +0530

    Comments

--- 01Basics/First.go
+++ 01Basics/First.go
@@ -7,3 +7,9 @@ import (
 func main() {
 	fmt.Println("hello world. i am learning golang!")
 }
+
+// Comments are written in go using double slash and multiline comments can be written in the following manner:-
+
+/* This is a multi line comment
+   This is the syntax for it
+*/
