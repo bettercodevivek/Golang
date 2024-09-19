776c87ee7798a667cbf74977853a6c249deed87a
bettercodevivek <viveksingh0520@gmail.com>
2024-09-19 12:46:45 +0530

    updation

--- 01Basics/First.go
+++ 01Basics/First.go
@@ -7,10 +7,20 @@ import (
 func main() {
 	fmt.Println("hello world. i am learning golang!")
 	fmt.Println("This is 2nd statement")
+
+	// Comments are written in go using double slash and multiline comments can be written in the following manner:-
+
+	/* This is a multi line comment
+	   This is the syntax for it
+	*/
+
+	// In Golang you can declare variables in the following 2 manners :-
+
+	// 1 . var variablename type = value
+
+	var myname string = "vivek"
+	fmt.Println(myname)
+
+	// In go, you can enclose string variables in double quotes only, single quotes will give error
+
 }
-
-// Comments are written in go using double slash and multiline comments can be written in the following manner:-
-
-/* This is a multi line comment
-   This is the syntax for it
-*/
