c006de57bdafdba627f6619595bb5c0be4e5e519
bettercodevivek <viveksingh0520@gmail.com>
2024-09-19 22:00:14 +0530

    Learning about constants

--- 01Basics/First.go
+++ 01Basics/First.go
@@ -96,4 +96,18 @@ func main() {
 
 	fmt.Println("hello " + name_1 + " I am " + name_2 + " and this is " + name_3)
 
+	//  If a variable should have a fixed value that cannot be changed, you can use the const keyword.
+
+	//  The const keyword declares the variable as "constant", which means that it is unchangeable and read-only.
+
+	const PI float32 = 3.14
+
+	fmt.Println(PI)
+
+	// There are 2 types of constants :- typed constants and untyped constants
+
+	// typed --- in which you declare a variable with defined type
+
+	// untyped --- in which you don't declare variable with defined type
+
 }
