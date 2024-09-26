ab5e1e948b6584115f81a5d746d9be1ab86231f2
bettercodevivek <viveksingh0520@gmail.com>
2024-09-26 13:02:53 +0530

    append func in slice

--- 02Basics/Second.go
+++ 02Basics/Second.go
@@ -49,4 +49,43 @@ func main() {
 
 	   However, unlike arrays, the length of a slice can grow and shrink as you see fit. */
 
+	myslice1 := []int{1, 2, 3, 4, 5, 6}
+
+	fmt.Println(myslice1)
+
+	/* In Go, there are two functions that can be used to return the length and capacity of a slice:
+
+	   len() function - returns the length of the slice (the number of elements in the slice)
+	   cap() function - returns the capacity of the slice (the number of elements the slice can grow or shrink to)
+
+	*/
+
+	// creating a slice from an array
+
+	myslice2 := arr2[0:2]
+
+	fmt.Println(myslice2)
+
+	// can also create slice using the make() function
+
+	// syntax =>  slice_name := make([]type, length, capacity)
+
+	myslice3 := make([]string, 4, 6)
+	myslice3[0] = "vivelk"
+	myslice3[1] = "vivelk"
+	myslice3[2] = "vivelk"
+	myslice3[3] = "vivelk"
+	fmt.Println(myslice3)
+
+	// So in the above case, capacity jo hai hamara slice ki woh 6 hai but length is 4, lekin 4 se zyda elements daalne mein index out of range
+	// error show ho raha hai, why is that???
+
+	// BECAUSE When you try to access myslice3[4] and myslice3[5] directly, it raises an "index out of range" error because those indices are beyond the current length of the slice.
+	//  Even though the slice has a capacity of 6, Go slices only allow you to access elements up to the current length. So in this case, your slice has a length of 4, which means valid indices are 0 to 3.
+
+	// TOH ISKA SOLUTION ??
+	// If you want to add elements beyond the initial length (4), you should use the append() function to grow the slice.
+
+	myslice3 = append(myslice3, "vivekf", "vivekn")
+	fmt.Println(myslice3)
 }
