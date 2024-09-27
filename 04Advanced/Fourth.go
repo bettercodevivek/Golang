8fc3b3ee7a7b449989c3fe87d66822a309072910
bettercodevivek <viveksingh0520@gmail.com>
2024-09-27 19:42:59 +0530

    learning recursion in go

--- 04Advanced/Fourth.go
+++ 04Advanced/Fourth.go
@@ -9,7 +9,21 @@ func factorial(n int) int {
 	return n * factorial(n-1)
 }
 
+func StringPrinter(str1 string, i int) string {
+	if i >= len(str1) {
+		return " "
+	}
+	fmt.Printf("%c", str1[i])
+
+	return StringPrinter(str1, i+1)
+}
+
 func main() {
 	// Let us try to understand recursion using the factorial example
 	fmt.Println(factorial(5))
+
+	var str string = "golang"
+
+	fmt.Println(StringPrinter(str, 0))
+
 }
