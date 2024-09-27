be178f2fa22eeb1dd14838cf59a6e19ee582e158
bettercodevivek <viveksingh0520@gmail.com>
2024-09-27 19:24:46 +0530

    Learning recursion in go

--- 04Advanced/Fourth.go
+++ 04Advanced/Fourth.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func factorial(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+	// Let us try to understand recursion using the factorial example
+	fmt.Println(factorial(5))
+}
