ad62e0407d7a2dc657aa8ebb89f38d7aedfd5a50
bettercodevivek <viveksingh0520@gmail.com>
2024-10-03 22:09:27 +0530

    Updations

--- 06Advanced/Sixth.go
+++ 06Advanced/Sixth.go
@@ -5,6 +5,13 @@ import "fmt"
 func main() {
 	var num1 int = 20
 
+	var arr1 = [...]int{1, 2, 3, 4, 5}
+
+	for i := 0; i < len(arr1); i++ {
+		adr1 := &arr1[i]
+		fmt.Println(*adr1)
+	}
+
 	var adr *int = &num1
 
 	fmt.Println(num1)
@@ -12,5 +19,7 @@ func main() {
 	*adr = num1 + 55
 
 	fmt.Println(*adr)
+
+	fmt.Println(arr1)
 
 }
