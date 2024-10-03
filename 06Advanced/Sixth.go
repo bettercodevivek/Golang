4c3d776839d27fe997f30da0450c4eecd00a2cc3
bettercodevivek <viveksingh0520@gmail.com>
2024-10-03 22:10:44 +0530

    Updation

--- 06Advanced/Sixth.go
+++ 06Advanced/Sixth.go
@@ -1,3 +1,13 @@
+/*Pointer: It's a variable that stores the memory address of another variable.
+ In simple words, it points to where some data is stored in the computer's memory.
+ Pointers are useful because they allow programs to:
+
+    Access and modify data efficiently: Instead of copying large amounts of data, you can just pass a pointer to the data, which saves memory and speeds up things.
+    Share data: Multiple parts of a program can work with the same data without creating multiple copies of it.
+	p := &x means the pointer p is storing the memory address of x (the location where x is stored).
+*p is called dereferencing the pointer, which means getting the value stored at the memory location p points to
+*/
+
 package main
 
 import "fmt"
