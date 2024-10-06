464d08ad13a71c9f41812407b57eb38da2d5c988
bettercodevivek <viveksingh0520@gmail.com>
2024-10-06 22:19:03 +0530

    Learning structs

--- 07Advanced/Seventh.go
+++ 07Advanced/Seventh.go
@@ -17,14 +17,24 @@ func main() {
 
 	A struct can be useful for grouping data together to create records.*/
 
-	var person1 person
 	//  var person2 person ;
 
-	person1.name = "vivek"
-	person1.age = 20
-	person1.address = "san francisco"
-	person1.orders = 40
+	// person1.name = "vivek"
+	// person1.age = 20
+	// person1.address = "san francisco"
+	// person1.orders = 40
 
-	fmt.Println(person1)
+	person1 := person{"vivek", 30, "pasadena", 50}
+
+	//With literal notation, you provide values for the fields in the order they are defined.
+
+	person2 := person{
+		name:    "johnny",
+		age:     30,
+		address: "san francisco",
+		orders:  100,
+	}
+
+	fmt.Println(person1, " ", person2)
 
 }
