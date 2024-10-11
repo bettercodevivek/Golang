28d109b24fe8a9c33b9a9b570db0f30536a46085
bettercodevivek <viveksingh0520@gmail.com>
2024-10-11 21:54:33 +0530

    Updations to backend code

--- 11Backend/Main.go
+++ 11Backend/Main.go
@@ -5,9 +5,22 @@ import (
 	"net/http"
 )
 
+type user struct {
+	firstname string
+	lastname  string
+	age       int
+}
+
 func main() {
+
+	user1 := user{"vivek", "singh", 22}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the root path of my server !")
+	})
+
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "user accessing this page is :- "+user1.firstname+user1.lastname)
 	})
 	var port string = ":8080"
 	fmt.Println("Server is up and running at :- ", port)
