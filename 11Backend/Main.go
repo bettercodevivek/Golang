3305c424012f3da30d1e9b1aace2750c04102d1b
bettercodevivek <viveksingh0520@gmail.com>
2024-10-12 13:03:25 +0530

    learning more about http servers

--- 11Backend/Main.go
+++ 11Backend/Main.go
@@ -19,13 +19,23 @@ func main() {
 
 	var user string = "/user"
 
+	var home string = "/home"
+
+	fs := http.FileServer(http.Dir("./Static"))
+
+	http.Handle(home, http.StripPrefix(home, fs))
+
 	http.HandleFunc(root, func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the root path of my server !")
 	})
 
 	http.HandleFunc(user, func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "user accessing this page is :- "+user1.firstname+user1.lastname)
+		fmt.Fprintf(w, "user accessing this page is :- %s %s", user1.firstname, user1.lastname)
 	})
+
+	// http.HandleFunc(home, func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "welcome home")
+	// })
 
 	var port string = ":8080"
 	fmt.Println("Server is up and running at :- ", port)
