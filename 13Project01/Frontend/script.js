b936153d020346b35f1f5f70740ed27b66d18adf
bettercodevivek <viveksingh0520@gmail.com>
2024-10-19 13:46:09 +0530

    Updation in javascript

--- 13Project01/Frontend/script.js
+++ 13Project01/Frontend/script.js
@@ -6,7 +6,16 @@ Form.addEventListener('submit', async function(event){
     const Password = document.getElementById('password').value;
     const Date = document.getElementById('date').value;
     
+    const response = await fetch("http://localhost:8080/register",{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify({Username,Password,Date})
+    })
     
+    const result = await response.json()
 
-} 
-    )
+   document.getElementById('responseMessage').textContent=result.message;
+
+});
