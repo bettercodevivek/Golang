ebf046ac7454f9cb631e76818e27bd95bd19c317
bettercodevivek <viveksingh0520@gmail.com>
2024-10-21 19:35:54 +0530

    Updation

--- 13Backend/Main.go
+++ 13Backend/Main.go
@@ -11,6 +11,27 @@ func main() {
 		http.ServeFile(w, r, "./index.html")
 	})
 
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Error occured, not the right http method :-", http.StatusMethodNotAllowed)
+			return
+		}
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "error parsing the form data :- ", http.StatusInternalServerError)
+			return
+		}
+
+		Username := r.FormValue("username")
+		Password := r.FormValue("password")
+		Email := r.FormValue("email")
+
+		fmt.Fprintf(w, "YOUR RESPONSE HAS BEEN RECORDED SUCCESSFULLY. HERE ARE THE DETAILS YOU ENTERED :-\n")
+		fmt.Fprintf(w, "Username :- %s\n", Username)
+		fmt.Fprintf(w, "Password :- %s\n", Password)
+		fmt.Fprintf(w, "Email :- %s\n", Email)
+	})
+
 	var port string = ":8080"
 
 	fmt.Printf("Server started at the port :-  %s", port)
