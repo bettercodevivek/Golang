d977d04d16a721ca6fa20ce3fc04075144b5663d
bettercodevivek <viveksingh0520@gmail.com>
2024-10-24 08:58:51 +0530

    updation in db integration

--- 14Backend/Main.go
+++ 14Backend/Main.go
@@ -10,7 +10,11 @@ import (
 
 func initDB() *sql.DB {
 	dsn := "root:Viveksequel24@tcp(127.0.0.1:3306)/viv1"
+	// Here dsn is the string that acts as a data source name which basically gives necessary information that helps to coonect with the database.
 	db, err := sql.Open("mysql", dsn)
+
+	// sql.Open() function takes 2 args , first one is the name of the driver in this case it is mysql whereas 2nd parameter is a dsn string.
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,25 +24,28 @@ func initDB() *sql.DB {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Connected to MYSQL Database Successfully")
+	fmt.Println("Successfully connected to datbase")
 
 	return db
 }
 
 func CreateUser(db *sql.DB, name string, age int) {
-	query := "INSERT INTO users(name,age)VALUES(?,?)"
+	query := "INSERT INTO users(name,age)values(?,?)"
 
-	_, err := db.Exec(query, name, age)
+	result, err := db.Exec(query, name, age)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	// result.LastInsertId()
-	// fmt.Printf("New user inserted with id :- %d\n", id)
+
+	id, _ := result.LastInsertId()
+	fmt.Printf("New user created with the id %d\n", id)
+
 }
 
 func main() {
 	db := initDB()
 	defer db.Close()
-	CreateUser(db, "alice", 30)
+
+	CreateUser(db, "Penny", 22)
 }
